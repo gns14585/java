50fad954c784309637dd7336fe8ec2cffb63896b
LeeJungHoon <gns14585@naver.com>
2023-08-28 11:43:37 +0900

    first

---
+++ src/ch05reference/C05array.java
@@ -0,0 +1,14 @@
+package ch05reference;
+
+public class C05array {
+	public static void main(String[] args) {
+		int[] a = { 3, 4, 5 };
+		int[] b = a;
+		
+		int[] c = { 10, 20, 30 };
+		int[] d = { 10, 20, 30 };
+		
+		System.out.println(c.equals(d));
+		System.out.println(c == d);
+	}
+}
---
+++ src/ch05reference/C06matrix.java
@@ -0,0 +1,43 @@
+package ch05reference;
+public class C06matrix {
+	public static void main(String[] args) {
+		int[][] a = 
+			{
+			{1,2,3}, 
+			{10,20,30}, 
+			{100,200,300 } 
+			};
+		
+		int[][] b = a;
+		
+		System.out.println(a[0][2]);
+		System.out.println(b[0][2]);
+		
+		System.out.println("- - - - - - - - - -");
+		
+		a[1][1] = 2000;
+		System.out.println(a[1][1]);
+		System.out.println(b[1][1]);
+		
+		System.out.println("- - - - - - - - - -");
+		
+		b[2][2] = 9999;
+		System.out.println(b[2][2]);
+		System.out.println(a[2][2]);
+		
+		System.out.println("- - - - - - - - - -");
+		
+		b[2] = new int[] { 111, 222, 333 };
+		System.out.println(b[2][2]);
+		System.out.println(a[2][2]);
+
+		System.out.println("- - - - - - - - - -");
+		
+		b = new int[][] {{33, 44},{22, 11}};
+		System.out.println(b[0][0]);
+		System.out.println(a[0][0]);
+		
+		System.out.println("- - - - - - - - - -");
+
+	}
+}
