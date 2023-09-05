371fe2af15e698766dc271a84899ac450596c3da
LeeJungHoon <gns14585@naver.com>
2023-09-05 17:48:30 +0900

    2023-09-05

--- src/c12api/lecture/C15indexOf.java
+++ src/c12api/lecture/C15indexOf.java
@@ -3,6 +3,7 @@ package c12api.lecture;
 public class C15indexOf {
     public static void main(String[] args) {
         // indexOf : 특정 문자열이 시작하는 위치(index) 리턴
+        /*
         String a = "spring";
 
         int ring = a.indexOf("ring");
@@ -17,5 +18,20 @@ public class C15indexOf {
         System.out.println(ring1);
 
         System.out.println(b.indexOf("ring", 3));
+        */
+
+        C15indexOf c = new C15indexOf();
+        c.countHi("ab hi hi");
+        System.out.println(c.countHi("ab hi ho"));
     }
+
+    public int countHi(String str) {
+        int count = 0;
+        int index1 = -1;
+        while((index1 = str.indexOf("hi", index1+1)) != -1) {
+            count++;
+        }
+        return count;
+    }
+
 }
