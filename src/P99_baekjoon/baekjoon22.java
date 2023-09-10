025a22b3b1ab1a2a4766e9dd9d24e5184cce6a46
이정훈 <gns14585@naver.com>
2023-09-10 23:04:11 +0900

    HOME.Practice

---
+++ src/P99_baekjoon/baekjoon22.java
@@ -0,0 +1,17 @@
+package P99_baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String c = a.trim();
+        String[] b = c.split(" ");
+        System.out.println(b.length);
+
+
+
+    }
+}
